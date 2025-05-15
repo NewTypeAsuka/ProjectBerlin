37f0ceeccd1267ee04664f2252914a45d9ec7c55
JUNBOOK\LeeSangJun <sangzoon0102@naver.com>
2025-05-15 15:55:47 +0900

    자바스크립트ㅡ 코드 수정

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 
 // 변수
+const gachaOnceBtn = document.getElementById("gacha-once-btn");
+const gachaTenBtn = document.getElementById("gacha-ten-btn");
 const bgm = document.getElementById("bgm");
 const soundToggleBtn = document.getElementById("sound-toggle-btn");
 
 // 화면 전환 함수
-function changeScreen(id) {
+function changeScreen(targetId) {
     const screens = document.querySelectorAll(".screen");
     screens.forEach(screen => {
         screen.classList.remove("active");
     });
 
-    const target = document.getElementById(id);
+    const target = document.getElementById(targetId);
     if (target) {
         target.classList.add("active");
     }
 }
 
 // 가챠 방식 버튼 핸들러(1회, 10회)
-document.getElementById("gacha-once-btn").addEventListener("click", () => {
+gachaOnceBtn.addEventListener("click", () => {
     // 나중에 필요한 로직 삽입 가능 (1회 뽑기 설정 등)
     changeScreen("step-two-page");
 });
-document.getElementById("gacha-ten-btn").addEventListener("click", () => {
+gachaTenBtn.addEventListener("click", () => {
     // 나중에 필요한 로직 삽입 가능 (10회 뽑기 설정 등)
     changeScreen("step-two-page");
 });
